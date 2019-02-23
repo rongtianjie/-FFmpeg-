b93a61008e9281327bdfa35e2ec287bbdbddf750
Johnny Rong <844877937@qq.com>
2019-02-23 13:00:21 -0800

    修改交互界面

--- README.md
+++ README.md
@@ -3,10 +3,19 @@
 基于 FFmpeg 的视频编码格式转换器，使用 Python 调用。
 
 ### 使用方法
-下载[FFmpeg](https://evermeet.cx/ffmpeg/ffmpeg-4.1.1.dmg)(链接版本v4.1.1)，下载完成后解压文件”ffmpeg“至 Home 文件夹下 ffmpeg 子文件夹中。Terminal 中运行 ffmpeg_converter.py 即可(其他环境下可能会报错)。
+下载[FFmpeg](https://evermeet.cx/ffmpeg/ffmpeg-4.1.1.dmg)(链接版本v4.1.1)，下载完成后解压文件 **ffmpeg** 至 Home 文件夹下 ffmpeg 子目录中。在 Downloads 下新建 ffmpeg_output 文件夹。Terminal 中运行 `python ffmpeg_converter.py` 即可(其他环境下可能会报错)。
+
+**由于 Shell 的限制，目前不支持文件名中包含空格的文件**
 
 ### 更新日志
 #### 2019.02.22 
 在 Terminal 实现基本功能，可设定转换参数有：编码器，编码速度，封装格式，输出文件名。
 
 未进行界面优化和注释。
+
+#### 2018.02.23
+改进交互界面，增加默认转换参数。（编码器: libx264, 编码速度: ultrafast, 封装格式: mp4）
+
+转换前显示源文件信息。
+
+添加部分注释。
--- ffmpeg_converter.py
+++ ffmpeg_converter.py
@@ -4,15 +4,16 @@
 Created on Fri Feb 22 14:01:26 2019
 
 @author: Johnny Rong
+
 """
 
 import subprocess
-import os
+import sys
 import time
 import tkinter as tk
 from tkinter import filedialog
 
-def select():
+def select():			# 源文件选择
     print ("Please select the origin video file: ")
     time.sleep(1)
     root = tk.Tk()
@@ -21,20 +22,24 @@ def select():
     print ("Source File: " + path)
     #print (origin_path)
     return path
-def encoder():
+def v_encoder():		# 视频编码器设置
+	print ("")
 	print ("Please choose the encoder:")
-	encoder_id = input("1. libx264 2.libx265 [1/2]? ")
+	print ("1. libx264 2.libx265")
+	encoder_id = input("[1/2]? ")
 
 	if encoder_id == "2":
 		encoder_str = "libx265"
 	else:
 		encoder_str = "libx264"
 	print("Video encoder set to '" + encoder_str +"'.")
-	return encoder_str
-def output():
+	return encoder_str #	
+def output():			# 封装格式设置
+	print ("")
 	format_str = input("Please input the format. Such as 'mp4' or 'avi' " )
-	return format_str
-def speed():
+	return format_str		
+def speed():			# 编码速度设置
+	print ("")
 	print ("Please choose the encode speed: ")
 	print ("1. Ultrafast 2. Veryfast 3. Medium 4. Slower 5. Placebo")
 	speed_id = input("[1~5]? ")
@@ -50,46 +55,79 @@ def speed():
 	else:
 		speed_str = "medium"
 	print("Convert speed set to '" + speed_str +"'.")
-	return speed_str
-def tune():
+	return speed_str		
+def tune():				# 编码优化设置(未使用)
 	print ("Please choose the tune: ")
 	print ("1. Flim 2. Animation 3. Grain 4. Fastdecode 5. Zerolatency")
 	tune_id = input("[1~5]? ")
 
-	if tune_id == 2:
+	if tune_id == "2":
 		tune_str = "animation"
-	elif tune_id == 3:
+	elif tune_id == "3":
 		tune_str = "grain"
-	elif tune_id == 4:
+	elif tune_id == "4":
 		tune_str = "fastdecode"
-	elif tune_id == 5:
+	elif tune_id == "5":
 		tune_str = "zerolatency"
 	else:
 		tune_str = "flim"
 
-	return tune_str
-def filename():
+	return tune_str			
+def filename():			# 输出文件名设置
 	filename_str = input ("Please input the output filename: ")
-	return filename_str
-def command():
+	return filename_str		
+def command():			# 指令生成
 	file_origin = select()
-	encoder_str = encoder()
-	format_str = output()
-	speed_str = speed()
-	#tune_str = tune()
+	time.sleep(0.5)
+	info = "~/ffmpeg/ffmpeg -i " + file_origin
+	subprocess.call(info, shell=True)
+	print ("")
+	print ("Do you want to convert the file?")
+	convert_confirm = input ("Contiune? [Y/N] ")
+	if convert_confirm == "y":
+		time.sleep(0.5)
+	else:
+		sys.exit()
+
+	v_encoder_str = "libx264" 	# 默认视频编码器
+	format_str = "mp4"			# 默认封装格式
+	speed_str = "ultrafast"		# 默认编码速度
+
+	parameter_id = "0"
+	while parameter_id != "4":
+		print ("")
+		print ("Please choose the parameter.")
+		print ("1. Video encoder (Default: libx264)") 	
+		print ("2. Format (Default: mp4)")				
+		print ("3. Convert speed (Default: ultrafast)") 
+		print ("4. Done")
+		parameter_id = input("[1~4]? ")
+		if parameter_id == "1":
+			v_encoder_str = v_encoder()
+		elif parameter_id == "2": 
+			format_str = output()
+		elif parameter_id == "3":
+			speed_str = speed()
+	
+	time.sleep(0.5)
+	print ("")
 	filename_str = filename()
-	time.sleep(1)
+
+	# 确认设置
+	time.sleep(0.5)
+	print ("")
 	print ("Confirmation:")
-	print ("You will convert the file to format '" + format_str + "' with encoder '" + encoder_str + "'and convert speed '" + speed_str + "'.")
+	print ("You will convert 'file_origin' to format '" + format_str + "' with encoder '" + v_encoder_str + "'and convert speed '" + speed_str + "'.")
 	confirm = input ("Contiune? [Y/N] ")
 	if confirm == "y":
 		time.sleep(0.5)
 	else:
-		os.system("pause")
+		sys.exit()
 
-	command_str = "~/ffmpeg/ffmpeg -i " + file_origin + " -c:v " + encoder_str + " -c:a copy -preset " + speed_str + " ~/Downloads/ffmpeg_output/" + filename_str + "." + format_str
+	# 生成指令
+	command_str = "~/ffmpeg/ffmpeg -i " + file_origin + " -c:v " + v_encoder_str + " -c:a copy -preset " + speed_str + " ~/Downloads/ffmpeg_output/" + filename_str + "." + format_str
 
-	return command_str
+	return command_str 		  		
 
 str = command()
 #print (str)
